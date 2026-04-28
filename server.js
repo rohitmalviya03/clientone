6d8b53d51082e6f77e571841d61ce1c264d740c8
Rohit Malviya <rmrohit439@gmail.com>
2026-04-28 13:14:16 +0530

    db chnages

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const dbPool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   port: Number(process.env.DB_PORT || 3306),
   user: process.env.DB_USER || "u201150341_spyrolab",
-  password: process.env.DB_PASS || "Spyrolab@rohit@0887",
+  password: process.env.DB_PASS || "Asd@088711",
   database: process.env.DB_NAME || "u201150341_lab_portal",
   waitForConnections: true,
   connectionLimit: 10
